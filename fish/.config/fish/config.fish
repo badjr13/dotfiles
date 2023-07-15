22074fd37eedcffae1eb0df5c2c56bd7742912d1
badjr13 <dorrance.bobby@gmail.com>
2023-07-14 22:02:48 -0400

    07-14-2023 Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,15 @@
 if status is-interactive
 
-# Vi Mode
-set fish_key_bindings fish_vi_key_bindings
+    # Vi Mode
+    set fish_key_bindings fish_vi_key_bindings
 
-# Alias
-alias cat="batcat"
-alias vim="/usr/local/bin/nvim"
-alias z="zellij"
+    # Alias
+    alias cat="batcat"
+    alias hx="/usr/local/bin/helix"
+    alias vim="/usr/local/bin/nvim"
+    alias z="zellij"
 
-# Starship Prompt - Keep at bottom
-starship init fish | source
+    # Starship Prompt - Keep at bottom
+    starship init fish | source
 
 end
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,2 @@
-vim.wo.number = true
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
