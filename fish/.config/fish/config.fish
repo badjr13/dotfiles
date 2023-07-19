e70cb575b1bd399adda9710540abe6a330a1d848
badjr13 <dorrance.bobby@gmail.com>
2023-07-19 13:16:59 -0400

    07-19-2023 Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,18 @@
 if status is-interactive
 
+    # Set Default Editor to Helix
+    set -Ux EDITOR "/usr/local/bin/helix"
     # Vi Mode
     set fish_key_bindings fish_vi_key_bindings
 
     # Alias
     alias cat="batcat"
+    alias dot="cd ~/dotfiles/"
     alias h="/usr/local/bin/helix"
     alias hx="/usr/local/bin/helix"
     alias vim="/usr/local/bin/helix"
     alias z="zellij"
+    alias zl="zellij --layout ~/dotfiles/zellij/.config/zellij/layouts/default.kdl"
 
     # Add Cargo to the PATH
     set -gx PATH $HOME/.cargo/bin $PATH
