fb7455c5cc09b482dc640963ed7abc462cc54fd2
Bobby Dorrance <bobby.dorrance@binarydefense.com>
2024-08-06 16:25:17 -0400

    Tue Aug 06 - 16:25:17

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,34 @@
 if status is-interactive
+
+    # *** Aliases ***
+    alias age="cd ~/workspaces/edr/agent"
+    alias bobs="cd ~/obsidian/badjr13"
+    alias cat="batcat"
+    alias dot="cd ~/dotfiles/"
+    alias hx="/usr/local/bin/helix"
+    alias ld="laydown"
+    alias obs="obsidian"
+    alias wobs="cd ~/obsidian/dorranceb1"
+    alias z="zellij --layout ~/.config/zellij/layouts/default.kdl"
+
+    # *** Functions ***
+    # Git commit with date
+    function gcd
+        set commit_message (date +"%a %b %d - %H:%M:%S")
+        git commit -m "$commit_message"
+    end
+
+    # *** Environment Variables ***
     # Vi Mode
     set fish_key_bindings fish_vi_key_bindings
 
     # Set Default Editor to Helix
     set -Ux EDITOR /usr/local/bin/helix
 
-    # Alias
-    alias age="cd ~/workspaces/edr/agent"
-    alias cat="batcat"
-    alias dot="cd ~/dotfiles/"
-    alias ld="laydown"
-    alias hx="/usr/local/bin/helix"
-    alias z="zellij --layout ~/.config/zellij/layouts/default.kdl"
-
     # Add llvm to the PATH
     set -gx PATH /usr/lib/llvm-14/bin $PATH
 
+    # *** Rust ***
     # Add Cargo to the PATH
     set -gx PATH $HOME/.cargo/bin $PATH
 
@@ -23,12 +36,13 @@ if status is-interactive
     set -gx CARGO_HOME $HOME/.cargo
     set -gx RUSTUP_HOME $HOME/.rustup
 
-    # Starship Prompt - Keep at bottom
-    starship init fish | source
-
-    # Go
+    # *** Go ***
     set PATH "$PATH:/usr/local/go/bin"
     set GOPATH "$HOME/go"
     set PATH "$PATH:$GOPATH/bin"
     set PATH "$PATH:/home/bobby/go/bin"
+
+    # *** Initializations ***
+    # Starship Prompt - Keep at bottom
+    starship init fish | source
 end
